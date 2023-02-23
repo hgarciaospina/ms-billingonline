ba0f3e1e3a9a42ddfe9fa32d936113c62b035791
hgarciaospina <henrygarciaospina@gmail.com>
2023-02-23 14:14:26 -0500

    se agrega servicio security-service

---
+++ security-service/src/main/java/academy/digitallab/store/security/repository/RolRepository.java
@@ -0,0 +1,14 @@
+package academy.digitallab.store.security.repository;
+
+import academy.digitallab.store.security.entity.Rol;
+import academy.digitallab.store.security.enums.RolNombre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
---
+++ security-service/src/main/java/academy/digitallab/store/security/service/RolService.java
@@ -0,0 +1,26 @@
+package academy.digitallab.store.security.service;
+
+import academy.digitallab.store.security.entity.Rol;
+import academy.digitallab.store.security.enums.RolNombre;
+import academy.digitallab.store.security.repository.RolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class RolService {
+
+    @Autowired
+    RolRepository rolRepository;
+
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return  rolRepository.findByRolNombre(rolNombre);
+    }
+
+    public void save(Rol rol){
+        rolRepository.save(rol);
+    }
+}
